68ae9a7c094a39ce56c0cc6e7141daa357f6acb0
Dylan Stankievech <dylan.stank@gmail.com>
2019-01-10 22:09:04 -0700

    Added a test for option long names

---
+++ test/options.test.js
@@ -0,0 +1,17 @@
+'ust strict';
+
+const expect = require('chai').expect;
+const allOptions = require('../lib/options').ALL_OPTIONS;
+
+describe.only('options', async function () {
+    describe('ALL_OPTIONS', async function () {
+        it('should include expected long names', async function () {
+            const longNames = ['number', 'evolved', 'type', 'unique', 'file', 'superEffective'];
+
+            longNames.forEach((longName) => {
+                expect(allOptions.filter(o => o.long === longName).length).to.be.eq(1);
+            });
+            expect(longNames.length).to.be.eq(allOptions.length);
+        });
+    });
+});
