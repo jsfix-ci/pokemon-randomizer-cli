c4b969edaf16d4ace3df1f2c99d390782232ead9
Dylan Stankievech <dylan.stank@gmail.com>
2021-07-21 21:44:50 -0600

    Use airbnb eslint rules, and some recommended

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    extends: [
+        'airbnb',
+        'plugin:chai-friendly/recommended',
+        'plugin:promise/recommended',
+        'plugin:mocha/recommended',
+    ],
+    plugins: [
+        'chai-friendly',
+        'promise',
+        'mocha',
+    ],
+    env: {
+        commonjs: true,
+        node: true,
+        mocha: true,
+    },
+    globals: {
+        expect: true,
+        chance: true,
+    },
+    rules: {
+        'max-len': ['error', 150],
+        indent: ['error', 4],
+        'indent': ['error', 4],
+        'linebreak-style': 0,
+        'func-names': 0,
+        'arrow-body-style': 0,
+        'no-unused-expressions': 0,
+        'chai-friendly/no-unused-expressions': 2,
+        "prefer-arrow-callback": 0,
+        "mocha/prefer-arrow-callback": 2,
+        'no-use-before-define': ['error', { variables: true, functions: false }],
+    },
+};
