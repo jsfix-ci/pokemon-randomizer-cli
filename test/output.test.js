ab4feca1232bb692b7568acf3c4277633e61306c
Dylan Stankievech <dylan.stank@gmail.com>
2021-07-21 22:08:42 -0600

    Fix logger and some tests

--- test/output.test.js
+++ test/output.test.js
@@ -8,18 +8,18 @@ const sinon = require('sinon');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
 const _ = require('lodash');
-const output = require('../lib/output');
+const proxyquire = require('proxyquire');
 
 chai.should();
 chai.use(sinonChai);
 
 describe('output', function () {
-    let stub;
+    let output;
+    let stubs;
     let options;
     let pokemon;
 
     beforeEach(function () {
-        stub = sinon.stub(output, 'logMessage');
         options = {
             number: chance.integer({ min: 1, max: 10 }),
         };
@@ -29,53 +29,59 @@ describe('output', function () {
                 name: chance.string(),
             });
         });
-    });
 
-    afterEach(function () {
-        stub.restore();
+        stubs = {
+            logger: {
+                log: sinon.stub(),
+            },
+        };
+
+        output = proxyquire('../lib/output', {
+            './logger': stubs.logger,
+        });
     });
 
     describe('logPokemon', function () {
         it('should call console.log per pokemon', async function () {
             await output.logPokemon(options, pokemon);
-            expect(stub).to.have.callCount(options.number + 1);
+            expect(stubs.logger.log).to.have.callCount(options.number + 1);
             _.forEach(pokemon, (poke) => {
-                expect(stub).to.have.been.calledWithExactly(poke.name);
+                expect(stubs.logger.log).to.have.been.calledWithExactly(poke.name);
             });
         });
 
         it('should output number of pokemon', async function () {
             await output.logPokemon(options, pokemon);
             const expected = `Picked ${options.number} Pokemon:`;
-            expect(stub).to.have.been.calledWithExactly(expected);
+            expect(stubs.logger.log).to.have.been.calledWithExactly(expected);
         });
 
         it('should output unique', async function () {
             options.unique = true;
             await output.logPokemon(options, pokemon);
             const expected = `Picked ${options.number}, unique Pokemon:`;
-            expect(stub).to.have.been.calledWithExactly(expected);
+            expect(stubs.logger.log).to.have.been.calledWithExactly(expected);
         });
 
         it('should output evolved', async function () {
             options.evolved = true;
             await output.logPokemon(options, pokemon);
             const expected = `Picked ${options.number}, evolved Pokemon:`;
-            expect(stub).to.have.been.calledWithExactly(expected);
+            expect(stubs.logger.log).to.have.been.calledWithExactly(expected);
         });
 
         it('should output type', async function () {
             options.type = chance.string();
             await output.logPokemon(options, pokemon);
             const expected = `Picked ${options.number}, ${options.type} Pokemon:`;
-            expect(stub).to.have.been.calledWithExactly(expected);
+            expect(stubs.logger.log).to.have.been.calledWithExactly(expected);
         });
 
         it('should output super effective against type', async function () {
             options.superEffective = chance.string();
             await output.logPokemon(options, pokemon);
             const expected = `Picked ${options.number}, super effective against ${options.superEffective} Pokemon:`;
-            expect(stub).to.have.been.calledWithExactly(expected);
+            expect(stubs.logger.log).to.have.been.calledWithExactly(expected);
         });
     });
 });
